ed9d1e66342e40fed518d7c910b6ed190d00545e
Sneha Mariam Sam <94544530+sneha-mariam-sam@users.noreply.github.com>
2026-02-05 13:42:51 +0100

    Refactor web scraping functions and improve error handling
    Refactor web scraping functions to improve structure and error handling. Update problem_1 to extract character attributes and handle requests more robustly. Implement problem_2_3 to scrape course details and save them to a CSV file.

--- web_scraping.py
+++ web_scraping.py
@@ -9,195 +9,179 @@ from pprint import pprint
 from typing import Dict, List, Union
 
 
+import csv
+import requests
+import bs4
+import re
+from pprint import pprint
+from typing import Dict, List, Union
+
+
+BASE_HIMYM_URL = "https://how-i-met-your-mother.fandom.com/wiki/"
+BASE_LSF_URL = (
+    "https://www.lsf.uni-saarland.de/qisserver/rds"
+    "?state=wtree&search=1&trex=step&root120221=320944%7C310559%7C318658%7C311255"
+    "&P.vx=kurz&lang=en&noDBAction=y&init=y&lang=en"
+)
+
+
 def problem_1(name: str) -> List[Dict[str, Union[str, List[str]]]]:
-    # TODO Write your code for Problem 1 here.
-    main_url = "https://how-i-met-your-mother.fandom.com/"
+    """Extract character attributes from HIMYM wiki."""
+    name_link = name.replace(" ", "_")
+    url = BASE_HIMYM_URL + name_link
 
-    name_link = name.replace(" ","_")
-    character_url = main_url + "/wiki/" + name_link #to obtain the link for each character
-    
-    character_result = requests.get(character_url)
-    character_soup = bs4.BeautifulSoup(character_result.text, 'lxml')
-    
-    infobox = character_soup.find("table", {"class": "infobox character"}) #filtering the infobox
-    
-    infobox_details = []
-    attributes = []
-    values = []
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+    except requests.RequestException:
+        print(f"Error fetching URL: {url}")
+        return []
 
-    tags = infobox.find_all("div")
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    infobox = soup.find("table", {"class": "infobox character"})
+    if not infobox:
+        return []
 
-    for tag in tags:
-        infobox_details.append(tag.text.strip().split("<a ")[0]) 
-    
-    for index in range(len(infobox_details)):
-        if index % 2 == 0:
-            attributes.append(infobox_details[index]) #assigning attributes
-        else:
-            values.append(re.sub("[\[].*?[\]]", "", infobox_details[index])) #assigning values
-
-    info_list=[]
-
-    for ind in range(len(attributes)):
-        info_list.append(dict(attribute = attributes[ind], value = values[ind])) #assigning key value pairs to list
+    info_list = []
+    for row in infobox.find_all("tr"):
+        header = row.find("th")
+        value = row.find("td")
+        if header and value:
+            # Clean text and remove references [1], [2], etc.
+            attr = header.get_text(strip=True)
+            val = re.sub(r"\[.*?\]", "", value.get_text(" ", strip=True))
+            info_list.append({"attribute": attr, "value": val})
 
     return info_list
-    
 
 
 def problem_2_1() -> List[Dict[str, str]]:
-    base_url = "https://www.lsf.uni-saarland.de/qisserver/rds?state=wtree&search=1&trex=step&root120221=320944%7C310559%7C318658%7C311255&P.vx=kurz&lang=en&noDBAction=y&init=y&lang=en"
+    """Get all courses and their URLs from LSF portal."""
+    try:
+        res = requests.get(BASE_LSF_URL, headers={"Accept-Language": "en-US,en;q=0.9"})
+        res.raise_for_status()
+    except requests.RequestException:
+        print("Error fetching LSF main page.")
+        return []
 
-    # TODO Write your code for Problem 2.1 here.
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    content_div = soup.find("div", {"class": "content_max_portal_qis"})
+    if not content_div:
+        return []
 
-    headers = {"Accept-Language": "en-US,en;q=0.9"} #to use only english version
-    base_result = requests.get(base_url, headers)
-    
-    base_soup = bs4.BeautifulSoup(base_result.text, 'lxml')
-    
-    content = base_soup.find("div", {"class": "content_max_portal_qis"})
-    
-    all_links = content.find_all("a", {"class": "ueb"})
+    all_links = content_div.find_all("a", {"class": "ueb"})
+    # Find link containing 'Alle Lehrveranstaltungen' dynamically
+    courses_url = None
+    for link in all_links:
+        if "Alle Lehrveranstaltungen" in link.get_text():
+            courses_url = link.get("href")
+            break
+    if not courses_url:
+        return []
 
-    all_courses_url = all_links[5].attrs['href'] #link with all courses
-    
-    course_result = requests.get(all_courses_url)
-    course_soup = bs4.BeautifulSoup(course_result.text, 'lxml')
+    try:
+        res_courses = requests.get(courses_url)
+        res_courses.raise_for_status()
+    except requests.RequestException:
+        print(f"Error fetching courses page: {courses_url}")
+        return []
 
-    each_course_table = course_soup.find_all("table", {"summary": "Übersicht über alle Veranstaltungen"})
-    
-    #tables 1:5, 2:8, 3:32, 4:32, 5:4, total:81
-    links=[]
-    courses=[]
-    for table in each_course_table:
-        tds = table.find_all("a", {"class": "regular"}) #finding links to each course
-        for td in tds:
-            courses.append(td.text.strip())
-            links.append(td.attrs['href'].strip())
+    soup_courses = bs4.BeautifulSoup(res_courses.text, "lxml")
+    tables = soup_courses.find_all("table", {"summary": "Übersicht über alle Veranstaltungen"})
+    course_list = []
 
-    course_list=[]
+    for table in tables:
+        for a_tag in table.find_all("a", {"class": "regular"}):
+            name = a_tag.get_text(strip=True)
+            url = a_tag.get("href", "").strip()
+            course_list.append({"Name of Course": name, "URL": url})
 
-    for ind in range(len(courses)):
-        course_dictionary = {'Name of Course': courses[ind], 'URL': links[ind]} #assigning key value pairs to dictionary
-        course_list.append(course_dictionary)
-        
     return course_list
 
 
 def problem_2_2(url: str) -> Dict[str, Union[str, List[str]]]:
-    # TODO Write your code for Problem 2.2 here.
+    """Extract course details from a course URL."""
+    try:
+        res = requests.get(url, headers={"Accept-Language": "en-US,en;q=0.9"})
+        res.raise_for_status()
+    except requests.RequestException:
+        print(f"Error fetching course page: {url}")
+        return {}
 
-    headers = {"Accept-Language": "en-US,en;q=0.9"} #to use only english version
-    course_result = requests.get(url, headers)
-    
-    course_soup = bs4.BeautifulSoup(course_result.text, 'lxml')
-    
-    info_table_headers = course_soup.find_all("table", {"summary": "Grunddaten zur Veranstaltung"}) #for basic information
-    
-    instructor_headers = course_soup.find_all("table", {"summary": "Verantwortliche Dozenten"}) #for responsible instructors
-    
-    attr = []
-    vals = []
-    add_links = []
-    all_instructors = []
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    info_table = soup.find("table", {"summary": "Grunddaten zur Veranstaltung"})
+    instructor_table = soup.find("table", {"summary": "Verantwortliche Dozenten"})
 
-    for head in info_table_headers:
-        lhs = head.find_all("th", "mod")
-        rhs = head.find_all("td", "mod_n_basic")
-    
-    for right in rhs:
-        header_string = ' '.join(map(str, right.get('headers'))) #to remove extra spaces
-        if header_string=="basic_12":
-            add_links.append(right.text.strip())
-        else:
-            inter = right.text.replace(u'\xa0',u' ').replace('\t','').replace('\n','').strip()
-            inter = re.sub(' +', ' ', right.text.replace(u'\xa0',u' ').replace('\t','').replace('\n','').strip())
-            vals.append(inter)
-            
-    vals.append(add_links)
+    data = {}
+    # Extract key-value pairs from info table
+    if info_table:
+        for row in info_table.find_all("tr"):
+            header = row.find("th")
+            value = row.find("td")
+            if header and value:
+                key = header.get_text(strip=True)
+                val = re.sub(r"\s+", " ", value.get_text(" ", strip=True))
+                data[key] = val
 
-    for left in lhs:
-        attr.append(left.text.strip())
-    
-    if(len(vals)!=len(attr)):
-        for right in rhs:
-            header_string = ' '.join(map(str, right.get('headers')))
-            if header_string=="basic_14":
-                attr.append("application period")
-    
-    
-    for ins in instructor_headers:
-        global instructor
-        global names
-    
-        instructor = ins.find("th", "mod").text.strip()
-        attr.append(instructor)
-        names = ins.find_all("td", {"class": ["mod_n_odd", "mod_n_even"]})
+    # Extract instructors
+    instructors = []
+    if instructor_table:
+        th = instructor_table.find("th", class_="mod")
+        if th:
+            key = th.get_text(strip=True)
+            data[key] = []
+        for td in instructor_table.find_all("td", class_=["mod_n_odd", "mod_n_even"]):
+            name = td.get_text(strip=True)
+            if name:
+                instructors.append(name)
+        if instructors:
+            data[key] = instructors
 
-        for name in names:
-            prof_name = name.text.replace('\xa0',' ').strip()
-            all_instructors.append(prof_name) 
-    
-    while('' in all_instructors) :
-        all_instructors.remove('')
-    
-    vals.append(all_instructors)
-
-    info_dictionary = {}
-
-    for ind in range(0,len(attr)):
-        info_dictionary[attr[ind]] = vals[ind] #assigning key value pairs to dictionary
-
-    return info_dictionary
+    return data
 
 
-def problem_2_3() -> None:
-    # TODO Write your code for Problem 2.3 here.
+def problem_2_3(output_file: str = "file.csv") -> None:
+    """Scrape all courses and their details, save to CSV."""
+    courses = problem_2_1()
+    if not courses:
+        print("No courses found.")
+        return
 
-    list_of_courses = problem_2_1()
-    list_details_dict = []
-    url_of_course = [] 
-    details_of_courses = {}
-    headers = []
+    # Determine full headers
+    headers = set()
+    course_details_list = []
 
-    f = open("file.csv", 'w', newline="")
-    
-    for ind in range(len(list_of_courses)): #obtaining name and url of courses from problem_2_1() and assigning to separate lists
-        dict = list_of_courses[ind]
-        first_pair = next(iter((dict.items())))
-        details_dict = {first_pair[0]:first_pair[1]}
-        list_details_dict.append(details_dict)
-        fieldnames = list(details_dict.keys())
-        url_of_course.append(dict.get('URL'))
+    for course in courses:
+        url = course.get("URL")
+        if not url:
+            continue
+        details = problem_2_2(url)
+        if not details:
+            continue
+        course_data = {**course, **details}
+        course_details_list.append(course_data)
+        headers.update(course_data.keys())
 
-    details_of_courses = problem_2_2(url_of_course[0]) #to obtain field names of basic information table
-    
-    details_dict.update(details_of_courses)
-    
-    headers = []
-    headers = [*details_of_courses] 
-    fieldnames.extend(headers) #total 16 fields
-
-    write = csv.writer(f)
-    write_dict = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
-    write_dict.writeheader()
-
-    for ind in range(0,len(url_of_course)):
-        details_of_courses = problem_2_2(url_of_course[ind])
-        list_details_dict[ind].update(details_of_courses)
-        write_dict.writerow(list_details_dict[ind])
-        
-    f.close()
-
-    return None
+    headers = list(headers)
+    with open(output_file, "w", newline="", encoding="utf8") as f:
+        writer = csv.DictWriter(f, fieldnames=headers, extrasaction="ignore")
+        writer.writeheader()
+        for course_data in course_details_list:
+            writer.writerow(course_data)
 
 
 def main():
-    # You can call your functions here to test their behaviours.
+    print("Example: HIMYM character attributes")
     pprint(problem_1("Lily Aldrin"))
-    #pprint(problem_2_1())
-    #pprint(problem_2_2("https://www.lsf.uni-saarland.de/qisserver/rds?state=verpublish&status=init&vmfile=no&publishid=136509&moduleCall=webInfo&publishConfFile=webInfo&publishSubDir=veranstaltung&noDBAction=y&init=y"))
-    #pprint(problem_2_3())
+
+    # print("Example: All courses")
+    # pprint(problem_2_1())
+
+    # print("Example: Single course details")
+    # pprint(problem_2_2("https://www.lsf.uni-saarland.de/qisserver/rds?state=verpublish&status=init&vmfile=no&publishid=136509&moduleCall=webInfo&publishConfFile=webInfo&publishSubDir=veranstaltung&noDBAction=y&init=y"))
+
+    # print("Scrape all courses to CSV")
+    # problem_2_3("courses.csv")
 
 
 
